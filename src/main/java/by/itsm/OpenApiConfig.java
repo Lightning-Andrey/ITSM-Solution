77f65bfe80d8ee57e7383c5e926220512c13e73a
Lightning-Andrey <andrejkamors@gmail.com>
2024-12-14 01:04:56 +0300

    Configured swagger ui

--- src/main/java/by/itsm/OpenApiConfig.java
+++ src/main/java/by/itsm/OpenApiConfig.java
@@ -3,7 +3,6 @@ package by.itsm;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -21,8 +20,7 @@ public class OpenApiConfig {
             new Info()
                 .title("ITSM-Solution Documentation")
                 .description("API documentation for the REST API project with Spring Security")
-                .version("0.0.1")
-                .license(new License().name("Apache 2.0").url("https://springdoc.org")))
+                .version("0.0.1"))
         .components(
             new Components()
                 .addSecuritySchemes(
